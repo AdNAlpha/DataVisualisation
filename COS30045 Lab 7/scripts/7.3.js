1026e71f6f4cdcba04c749293b51266d49819408
AdNAlpha <adnaanshaikh1@gmail.com>
2024-10-12 23:07:41 +0800

    comments

--- COS30045 Lab 7/scripts/7.3.js
+++ COS30045 Lab 7/scripts/7.3.js
@@ -3,6 +3,7 @@ function init() {
     var h = 200;
     var margin = {top: 20, right: 30, bottom: 40, left: 30};    
     
+    //Datset
     var dataset = [
         {apples: 5, oranges: 10, grapes: 22},
         {apples: 4, oranges: 12, grapes: 28},
@@ -13,10 +14,12 @@ function init() {
 
     var keys = ["apples", "oranges", "grapes"];
 
+    // series
     var series = d3.stack()
         .keys(keys)
         (dataset);
 
+    //SVG Body
     var svg = d3.select("body")
         .append("svg")
         .attr("width", w + margin.left + margin.right)
@@ -24,17 +27,21 @@ function init() {
         .append("g")
         .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
+    //xScale
     var xScale = d3.scaleBand()
         .domain(d3.range(dataset.length))
         .range([0, w])
         .padding(0.1);
 
+    //yScale
     var yScale = d3.scaleLinear()
         .domain([0, d3.max(dataset, function(d) { return d.apples + d.oranges + d.grapes; })])
         .range([h, 0]);
 
+    //Color
     var color = d3.scaleOrdinal(d3.schemeCategory10);
 
+    // Groups SetUp
     var groups = svg.selectAll("g.layer")
         .data(series)
         .enter()
@@ -44,6 +51,7 @@ function init() {
             return color(i);
         });
 
+    // Rect SetUp
     groups.selectAll("rect")
         .data(function(d) { return d; })
         .enter()
