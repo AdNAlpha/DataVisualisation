89b855355101146e01fae074c0c0488e6f0ef178
AdNAlpha <adnaanshaikh1@gmail.com>
2024-10-12 23:05:58 +0800

    semi fixed bar chart

--- COS30045 Lab 7/scripts/7.3.js
+++ COS30045 Lab 7/scripts/7.3.js
@@ -1,79 +1,59 @@
-    function init() {
-        var w = 600;
-        var h = 300;
+function init() {
+    var w = 300;
+    var h = 200;
+    var margin = {top: 20, right: 30, bottom: 40, left: 30};    
+    
+    var dataset = [
+        {apples: 5, oranges: 10, grapes: 22},
+        {apples: 4, oranges: 12, grapes: 28},
+        {apples: 2, oranges: 19, grapes: 32},
+        {apples: 7, oranges: 23, grapes: 35},
+        {apples: 23, oranges: 17, grapes: 43},
+    ];
 
-        // Dataset
-        var dataset = [
-            {apples: 5, oranges: 10, grapes: 22},
+    var keys = ["apples", "oranges", "grapes"];
 
-            {apples: 4, oranges: 12, grapes: 28},
+    var series = d3.stack()
+        .keys(keys)
+        (dataset);
 
-            {apples: 2, oranges: 19, grapes: 32},
+    var svg = d3.select("body")
+        .append("svg")
+        .attr("width", w + margin.left + margin.right)
+        .attr("height", h + margin.top + margin.bottom)
+        .append("g")
+        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-            {apples: 7, oranges: 23, grapes: 35},
-
-            {apples: 23, oranges: 17, grapes: 43},
-        ];
-
-        // Stack Set up
-        var stack= d3.stack()
-                    .keys(["apples", "oranges", "grapes"]);
-                
-        // Genrate the Series
-        var series = stack(dataset);
-
-        // xScale
-        var xScale = d3.scaleBand()
+    var xScale = d3.scaleBand()
         .domain(d3.range(dataset.length))
         .range([0, w])
         .padding(0.1);
 
-        // yScale
-        var yScale = d3.scaleLinear() // Corrected function call
-            .domain([0, d3.max(dataset, function(d) {
-                return d.apples + d.oranges + d.grapes; // Total height for y scale
-            })])
-            .range([h, 0]);
+    var yScale = d3.scaleLinear()
+        .domain([0, d3.max(dataset, function(d) { return d.apples + d.oranges + d.grapes; })])
+        .range([h, 0]);
 
-        //Color
-        var color = d3.scaleOrdinal(d3.schemeCategory10)
+    var color = d3.scaleOrdinal(d3.schemeCategory10);
 
-        // SVG 
-        var svg = d3.select("body")
-                    .append("svg")
-                    .attr("width", w)
-                    .attr("height", h)
-        
-        // Groups set up 
-        var groups = svg.selectAll("g")
-                        .data(series)
-                        .enter()
-                        .append("g")
-                        .style("fill", function(d, i) {
-                            return color(i); // apply color to each group
-                        });
-                        //.attr("transform", function(d, i) {
-                          //  return "translate(" + xScale(i) + ",0)"; // position the groups according to the xScale
-                        //});
+    var groups = svg.selectAll("g.layer")
+        .data(series)
+        .enter()
+        .append("g")
+        .attr("class", "layer")
+        .style("fill", function(d, i) {
+            return color(i);
+        });
 
-        // Rects
-        var rects = groups.selectAll("rect")
-                        .data(function(d) {return d;})
-                        .enter()
-                        .append("rect")
-                        .attr("x", function(d, i) {
-                            return xScale(i);
-                        })
+    groups.selectAll("rect")
+        .data(function(d) { return d; })
+        .enter()
+        .append("rect")
+        .attr("x", function(d, i) { return xScale(i); })
+        .attr("y", function(d) { return yScale(d[1]); })
+        .attr("height", function(d) { return yScale(d[0]) - yScale(d[1]); })
+        .attr("width", xScale.bandwidth());
 
-                        .attr("y", function(d, i) {
-                            return yScale(d[1]);
-                        })
 
-                        .attr("height",function(d) {
-                            return yScale(d[0] - yScale(d[1]));
-                        })
+}
 
-                        .attr("width", xScale.bandwidth());
-    }
-
-    window.onload = init;
+window.onload = init;
