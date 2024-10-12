3df01b4e6f013d8ad9b464ae3927e8c4ee00f78d
AdNAlpha <adnaanshaikh1@gmail.com>
2024-10-12 17:53:27 +0800

    Pie chart completed

--- COS30045 Lab 7/scripts/7.2.js
+++ COS30045 Lab 7/scripts/7.2.js
@@ -1,8 +1,9 @@
 function init() {
     var w = 600;
     var h = 300;
-    var outerRadius = w /2;
-    var innerRadius = 0;
+    var outerRadius = w / 4; // Small pie chart
+    var innerRadius = 65; // change to non zero to give Donut effect 
+
 
     var dataset = [5, 10, 20, 45, 6, 25];
 
@@ -10,28 +11,38 @@ function init() {
 
     var svg = d3.select("body")
                 .append("svg")
-                .attr("width" ,w)
-                .attr ("height", h);
+                .attr("width", w)
+                .attr("height", h);
 
     var arc = d3.arc()
             .outerRadius(outerRadius)
             .innerRadius(innerRadius);
 
-    var pie = d3.pie()
+    var pie = d3.pie();
 
     var arcs = svg.selectAll("g.arc")
                 .data(pie(dataset))
                 .enter()
                 .append("g")
                 .attr("class", "arc")
-                .attr("transform", "translate("+ outerRadius + "," + outerRadius + ")");
+                .attr("transform", "translate(" + w / 2 + "," + h / 2 + ")"); // Center the pie chart
 
     arcs.append("path")
-    .attr("fill", function(d, i) {
-        return color(i);
-    })
-    .attr("d", arc);
+        .attr("fill", function(d, i) {
+            return color(i);
+        })
+        .attr("d", arc); // define the shape of the arc
 
-    
+    arcs.append("text")
+        .attr("transform", function(d) {
+            // It Pushes the text outwards by finding the middle of an irregular shape
+            var centroid = arc.centroid(d);
+            return "translate(" + arc.centroid(d) + ")";
+        })
+        .attr("text-anchor", "middle")
+        .text(function(d) {
+            return d.value;
+        });
 }
+
 window.onload = init;
